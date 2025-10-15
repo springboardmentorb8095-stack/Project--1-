77a833aee6567e2ae9ef525c2d8bd0a48335322a
Kumarankit666 <ankitkr816102@gmail.com>
2025-10-15 14:50:30 +0530

    Deleted unwanted files

--- Harshada/backend/users/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Profile
-from .models import ProfileClient, ProfileFreelancer
-
-admin.site.register(Profile)
-admin.site.register(ProfileFreelancer)
-admin.site.register(ProfileClient)
